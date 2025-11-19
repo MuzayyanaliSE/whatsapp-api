e713f14cd7c6044f5de520b6176c0101caf80818
MuzayyanAliSE <muzayyannust1@hotmail.com>
2025-11-19 13:39:18 +0500

    fix : Fixed

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as bodyParser from 'body-parser';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
   const config = new DocumentBuilder()
     .setTitle('WhatsApp API')
